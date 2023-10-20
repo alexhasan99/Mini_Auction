f78bca5d9357ad46faf24553b489e037e9ec9342
alexhasan99 <mohammad.hasan19999@gmail.com>
2023-10-20 14:33:00 +0200

    ad

---
+++ Mini_Auction/Persistence/AuctionDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mini_Auction.Persistence
+{
+    public class AuctionDbContext : DbContext
+    {
+        public AuctionDbContext(DbContextOptions<AuctionDbContext> options) : base(options) { }
+
+        public DbSet<AuctionDB> Auctions { get; set; }
+
+        public DbSet<BidDB> Bids { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AuctionDB>().HasData(
+                new AuctionDB
+                {
+
+                })
+        }
+    }
+}
